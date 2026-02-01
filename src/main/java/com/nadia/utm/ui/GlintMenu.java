5d755f55cf2d27b2cd4e6929289a6fbbd1a5b2e2
nadia8666 <115858781+nadia8666@users.noreply.github.com>
2026-02-01 11:22:36 -0800

    fix slot in out

--- src/main/java/com/nadia/utm/ui/GlintMenu.java
+++ src/main/java/com/nadia/utm/ui/GlintMenu.java
@@ -34,11 +34,31 @@ public class GlintMenu extends AbstractContainerMenu {
         addPlayerHotbar(inv);
 
         this.addSlot(new SlotItemHandler(this.blockEntity.inventory, 0, 8, 10));
-        this.addSlot(new SlotItemHandler(this.blockEntity.inventory, 1, 34, 10));
+        this.addSlot(new SlotItemHandler(this.blockEntity.inventory, 1, 34, 10) {
+            @Override
+            public boolean mayPlace(@NotNull ItemStack stack) {
+                return false;
+            }
+
+            @Override
+            public void onTake(@NotNull Player player, @NotNull ItemStack stack) {
+                ItemStack inputStack = GlintMenu.this.blockEntity.inventory.getStackInSlot(0);
+
+                if (!inputStack.isEmpty()) {
+                    inputStack.shrink(1);
+                    GlintMenu.this.blockEntity.inventory.setStackInSlot(0, inputStack);
+                }
+
+                // set output glint
+
+                super.onTake(player, stack);
+            }
+        });
     }
 
 
     // slots 36+ = TileInventory slots
+    public static final int INPUT_SLOT_INDEX = 36;
     private static final int HOTBAR_SLOT_COUNT = 9;
     private static final int PLAYER_INVENTORY_ROW_COUNT = 3;
     private static final int PLAYER_INVENTORY_COLUMN_COUNT = 9;
@@ -48,6 +68,7 @@ public class GlintMenu extends AbstractContainerMenu {
     private static final int TE_INVENTORY_FIRST_SLOT_INDEX = VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT;
 
     private static final int TE_INVENTORY_SLOT_COUNT = 2;  // item in , item out
+
     @Override
     public @NotNull ItemStack quickMoveStack(@NotNull Player playerIn, int pIndex) {
         Slot sourceSlot = slots.get(pIndex);
@@ -56,8 +77,7 @@ public class GlintMenu extends AbstractContainerMenu {
         ItemStack copyOfSourceStack = sourceStack.copy();
 
         if (pIndex < VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT) {
-            if (!moveItemStackTo(sourceStack, TE_INVENTORY_FIRST_SLOT_INDEX, TE_INVENTORY_FIRST_SLOT_INDEX
-                    + TE_INVENTORY_SLOT_COUNT, false)) {
+            if (!moveItemStackTo(sourceStack, INPUT_SLOT_INDEX, INPUT_SLOT_INDEX + 1, false)) {
                 return ItemStack.EMPTY;  // EMPTY_ITEM
             }
         } else if (pIndex < TE_INVENTORY_FIRST_SLOT_INDEX + TE_INVENTORY_SLOT_COUNT) {
@@ -73,6 +93,7 @@ public class GlintMenu extends AbstractContainerMenu {
         } else {
             sourceSlot.setChanged();
         }
+
         sourceSlot.onTake(playerIn, sourceStack);
         return copyOfSourceStack;
     }
@@ -84,22 +105,16 @@ public class GlintMenu extends AbstractContainerMenu {
     }
 
     private void addPlayerInventory(Inventory playerInventory) {
-        for (int i = 0; i < 3; ++i) {
-            for (int l = 0; l < 9; ++l) {
-                this.addSlot(new Slot(playerInventory, l + i * 9 + 9, 8 + l * 18, 84 + i * 18));
+        for (int y = 0; y < 3; ++y) {
+            for (int x = 0; x < 9; ++x) {
+                this.addSlot(new Slot(playerInventory, x + y * 9 + 9, 8 + x * 18, 84 + y * 18));
             }
         }
     }
 
     private void addPlayerHotbar(Inventory playerInventory) {
-        for (int i = 0; i < 9; ++i) {
-            this.addSlot(new Slot(playerInventory, i, 8 + i * 18, 142));
+        for (int x = 0; x < 9; ++x) {
+            this.addSlot(new Slot(playerInventory, x, 8 + x * 18, 142));
         }
-    }
-
-    @Override
-    public boolean canDragTo(Slot slot) {
-        utm.LOGGER.info("[UTM] {}", slot.index);
-        return slot.index == 35;
     }
 }
