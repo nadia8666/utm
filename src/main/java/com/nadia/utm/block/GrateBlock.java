f0017799d2acce1145c7a96f4feb9647f27d223d
nadia8666 <115858781+nadia8666@users.noreply.github.com>
2026-02-01 17:03:19 -0800

    waterloggable grates

--- src/main/java/com/nadia/utm/block/GrateBlock.java
+++ src/main/java/com/nadia/utm/block/GrateBlock.java
@@ -9,29 +9,57 @@ import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 
-public class GrateBlock extends Block implements IWrenchable {
+public class GrateBlock extends Block implements IWrenchable, SimpleWaterloggedBlock {
     public static DirectionProperty VERTICAL_DIRECTION = DirectionProperty.create("vertical_direction", Direction.UP, Direction.DOWN);
     private static final VoxelShape BOTTOM;
     private static final VoxelShape TOP;
+    public static BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 
     public GrateBlock(Properties properties) {
         super(properties);
 
-        registerDefaultState(this.stateDefinition.any().setValue(VERTICAL_DIRECTION, Direction.UP));
+        registerDefaultState(this.stateDefinition.any().setValue(VERTICAL_DIRECTION, Direction.UP).setValue(WATERLOGGED, false));
+    }
+
+    @Override
+    protected @NotNull FluidState getFluidState(BlockState state) {
+        return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
-        pBuilder.add(VERTICAL_DIRECTION);
+        pBuilder.add(VERTICAL_DIRECTION, WATERLOGGED);
     }
+
+    @Override
+    protected @NotNull BlockState updateShape(
+            BlockState state,
+            @NotNull Direction direction,
+            @NotNull BlockState neighborState,
+            @NotNull LevelAccessor level,
+            @NotNull BlockPos pos,
+            @NotNull BlockPos neighborPos) {
+        if (state.getValue(WATERLOGGED)) {
+            level.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
+        }
+        return super.updateShape(state, direction, neighborState, level, pos, neighborPos);
+    }
+
+
 
     protected @NotNull VoxelShape getShape(@NotNull BlockState state, @NotNull BlockGetter level, @NotNull BlockPos pos, @NotNull CollisionContext context) {
         return state.getValue(VERTICAL_DIRECTION).getAxisDirection() == Direction.AxisDirection.NEGATIVE ? BOTTOM : TOP;
