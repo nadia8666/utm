66a2f397a92039af9587e434470ed6c16af6afb9
pikmi <pikmiadventurer@gmail.com>
2026-02-08 19:21:46 -0500

    interdictor exists 2

--- src/main/java/com/nadia/utm/block/InterdictorBlock.java
+++ src/main/java/com/nadia/utm/block/InterdictorBlock.java
@@ -1,9 +1,21 @@
 package com.nadia.utm.block;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RedstoneLampBlock;
+import net.minecraft.world.level.block.state.BlockState;
+
 
 public class InterdictorBlock extends RotatableBlock {
     public InterdictorBlock(Properties properties) {
         super(properties);
+    }
+
+    protected void neighborChanged(BlockState state, Level level, BlockPos pos, Block block, BlockPos fromPos, boolean isMoving) {
+        if (!level.isClientSide)  { // also needs redstone signal detect
+            // apply one stack of Interdicted! to all players nearby
+        }
+
     }
 }
