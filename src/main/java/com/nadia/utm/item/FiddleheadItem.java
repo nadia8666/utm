9e824dd0975f7126058480296d48448149580ec8
pikmi <pikmiadventurer@gmail.com>
2026-02-11 00:03:53 -0500

    fiddelehad buff becuas enadia says it bad (true though)

--- src/main/java/com/nadia/utm/item/FiddleheadItem.java
+++ src/main/java/com/nadia/utm/item/FiddleheadItem.java
@@ -5,6 +5,7 @@ import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.common.ItemAbility;
 import org.jetbrains.annotations.NotNull;
 
@@ -22,10 +23,13 @@ public class FiddleheadItem extends Item {
             var hi = player.getLookAngle();
             var mult = 0.5;
             if (player.isFallFlying() & (player.position().y > 512)) {
-              mult=15;
+              mult=20;
             }
             //  player.startUsingItem(hand);
-            player.setDeltaMovement(hi.x * mult, hi.y * Math.min(mult,1), hi.z * mult);
+            var hi2 = player.getDeltaMovement();
+            var hi3 = new Vec3(hi.x * mult, hi.y * Math.min(mult,1), hi.z * mult);
+            var hi4 = hi2.add(hi3);
+            player.setDeltaMovement(hi4.x,hi4.y,hi4.z);
             itemstack.setDamageValue(itemstack.getDamageValue()+1);
             return InteractionResultHolder.consume(itemstack);
         } else {
