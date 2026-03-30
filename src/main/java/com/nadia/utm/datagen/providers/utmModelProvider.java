f516d6d3ec0b21e8959ee23b11a4eb0baedde5e8
nadia <115858781+nadia8666@users.noreply.github.com>
2026-03-29 22:57:26 -0700

    fix formatting

--- src/main/java/com/nadia/utm/datagen/providers/utmModelProvider.java
+++ src/main/java/com/nadia/utm/datagen/providers/utmModelProvider.java
@@ -5,10 +5,9 @@ import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
 public class utmModelProvider extends BlockStateProvider {
-   public utmModelProvider(PackOutput output, ExistingFileHelper helper) {
-       super(output, "utm", helper);
-   }
-
+    public utmModelProvider(PackOutput output, ExistingFileHelper helper) {
+        super(output, "utm", helper);
+    }
 
     @Override
     protected void registerStatesAndModels() {
