42e8df4c58d5f0f7fa281802ecf0d3fa4fab9715
nadia <115858781+nadia8666@users.noreply.github.com>
2026-03-29 22:27:10 -0700

    wip launc contrpation

---
+++ src/main/java/com/nadia/utm/client/ui/LaunchScreen.java
@@ -0,0 +1,18 @@
+package com.nadia.utm.client.ui;
+
+import com.nadia.utm.gui.LaunchMenu;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Inventory;
+
+public class LaunchScreen extends AbstractContainerScreen<LaunchMenu> {
+    public LaunchScreen(LaunchMenu menu, Inventory playerInventory, Component title) {
+        super(menu, playerInventory, title);
+    }
+
+    @Override
+    protected void renderBg(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY) {
+
+    }
+}
---
+++ src/main/java/com/nadia/utm/gui/LaunchMenu.java
@@ -0,0 +1,29 @@
+package com.nadia.utm.gui;
+
+import com.nadia.utm.registry.ui.utmMenus;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+public class LaunchMenu extends AbstractContainerMenu {
+    public LaunchMenu(int containerId, Inventory inv, FriendlyByteBuf extraData) {
+        this(containerId, inv);
+    }
+
+    public LaunchMenu(int containerId, Inventory inv) {
+        super(utmMenus.GLINT_MENU.get(), containerId);
+    }
+
+    @Override
+    public @NotNull ItemStack quickMoveStack(@NotNull Player player, int index) {
+        return null;
+    }
+
+    @Override
+    public boolean stillValid(@NotNull Player player) {
+        return false;
+    }
+}
