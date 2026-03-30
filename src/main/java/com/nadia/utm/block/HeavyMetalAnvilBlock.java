5e1daa179c94b5117d221591b26545f65002eb8a
nadia <115858781+nadia8666@users.noreply.github.com>
2026-03-29 22:54:39 -0700

    fix hma damage

--- src/main/java/com/nadia/utm/block/HeavyMetalAnvilBlock.java
+++ src/main/java/com/nadia/utm/block/HeavyMetalAnvilBlock.java
@@ -1,15 +1,15 @@
 package com.nadia.utm.block;
 
+import com.nadia.utm.registry.block.utmBlocks;
 import net.minecraft.world.level.block.AnvilBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class HeavyMetalAnvilBlock extends AnvilBlock {
-    private static final float FALL_DAMAGE_PER_DISTANCE = 0F;
     public HeavyMetalAnvilBlock(Properties properties) {
         super(properties);
     }
 
     public static BlockState damage(BlockState state) {
-        return state;
+        return utmBlocks.HEAVY_METAL_ANVIL.block.get().defaultBlockState().setValue(FACING, state.getValue(FACING));
     }
 }
