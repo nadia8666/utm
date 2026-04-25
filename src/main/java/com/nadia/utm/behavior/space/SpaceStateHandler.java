0245bd19eb89e2dfc0c930270f6d6c95daeda3e2
nadia <115858781+nadia8666@users.noreply.github.com>
2026-04-25 16:58:58 -0700

    rewrite contraption launching

--- src/main/java/com/nadia/utm/behavior/space/SpaceStateHandler.java
+++ src/main/java/com/nadia/utm/behavior/space/SpaceStateHandler.java
@@ -5,9 +5,11 @@ import com.nadia.utm.event.utmEvents;
 import com.nadia.utm.networking.payloads.LaunchContraptionPayload;
 import com.nadia.utm.registry.dimension.utmDimensions;
 import com.nadia.utm.registry.enchantment.utmEnchantments;
+import com.nadia.utm.utm;
 import com.simibubi.create.content.contraptions.AbstractContraptionEntity;
+import io.netty.buffer.Unpooled;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.TickTask;
 import net.minecraft.server.level.ServerLevel;
@@ -25,13 +27,14 @@ import net.minecraft.world.level.portal.DimensionTransition;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.event.entity.living.LivingEvent;
+import net.neoforged.neoforge.event.entity.living.LivingFallEvent;
 import net.neoforged.neoforge.event.tick.EntityTickEvent;
 import net.neoforged.neoforge.event.tick.PlayerTickEvent;
+import net.neoforged.neoforge.network.connection.ConnectionType;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import javax.annotation.Nullable;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static com.nadia.utm.registry.attachment.utmAttachments.ENTERED_2313AG;
@@ -73,12 +76,6 @@ public class SpaceStateHandler {
         }
     }
 
-    static {
-        utmEvents.register(PlayerTickEvent.Post.class, SpaceStateHandler::onPlayerTick);
-        utmEvents.register(EntityTickEvent.Post.class, SpaceStateHandler::onEntityTick);
-        utmEvents.register(LivingEvent.LivingJumpEvent.class, SpaceStateHandler::onJump);
-    }
-
     public static final Set<Block> UNMODIFIED_BLOCKS = Set.of(
             Blocks.STONE,
             Blocks.COBBLESTONE,
@@ -98,78 +95,125 @@ public class SpaceStateHandler {
         ServerLevel overworld = server.getLevel(Level.OVERWORLD);
         if (ag == null || overworld == null) return;
 
-        AbstractContraptionEntity contraption = null;
+        AbstractContraptionEntity contraption;
         Entity vehicle = player.level().getEntity(payload.id());
         if (vehicle instanceof AbstractContraptionEntity c)
             contraption = c;
+        else {
+            contraption = null;
+        }
 
         if (contraption == null) return;
 
         ServerLevel target = vehicle.level() == overworld ? ag : overworld;
+        ServerLevel origin = vehicle.level() == overworld ? overworld : ag;
+
         if (vehicle.level() == target) return;
 
-        if (player.level() == ag && player.getVehicle() == vehicle)
-            player.unRide();
+        Map<UUID, Integer> passengers = new HashMap<>(contraption.getContraption().getSeatMapping());
+        for (UUID uuid : passengers.keySet()) {
+            Entity e = origin.getEntity(uuid);
+            if (e == null) continue;
 
-        List<Entity> passengers = new ArrayList<>(List.copyOf(contraption.getPassengers()));
-
-        if (player.level() == ag)
-            passengers.remove(player);
+            if (e.level() == ag) {
+                passengers.remove(uuid);
+                e.unRide();
+            }
+        }
 
         AABB bounds = contraption.getBoundingBox();
-        double yOffset = (319 + bounds.getYsize()) - contraption.getY();
-
-        CompoundTag nbt = contraption.getContraption().writeNBT(contraption.registryAccess(), false);
+        double yOffset = (target.getLogicalHeight() + 512 + bounds.getYsize()) - contraption.getY();
 
         Entity cVehicle = contraption.getVehicle();
+        if (cVehicle == null) return;
+
         AtomicReference<Entity> finalVehicle = new AtomicReference<>(null);
+        RegistryFriendlyByteBuf buf = new RegistryFriendlyByteBuf(Unpooled.buffer(), target.registryAccess(), ConnectionType.OTHER);
+        contraption.writeSpawnData(buf);
+
         contraption.getContraption().getBlocks().clear();
-        contraption.changeDimension(new DimensionTransition(
+        contraption.getContraption().disassembled = true;
+        contraption.ejectPassengers();
+        contraption.stopRiding();
+
+        cVehicle.changeDimension(new DimensionTransition(
                 target,
-                contraption.position().add(0, yOffset, 0),
-                contraption.getDeltaMovement(),
-                contraption.getYRot(),
-                contraption.getXRot(),
-                (newEntity) -> {
-                    if (newEntity instanceof AbstractContraptionEntity newContraption) {
-                        newContraption.getContraption().readNBT(ag, nbt, true);
-                        player.getServer().tell(new TickTask(player.getServer().getTickCount() + 20, () -> {
-                            Entity targVehicle = finalVehicle.get();
-                            if (targVehicle != null) {
-                                newContraption.startRiding(targVehicle);
-
-                                int index = 0;
-                                for (Entity pass : passengers) {
-                                    newContraption.addSittingPassenger(pass, index);
-                                    index++;
-                                }
-
-                                newContraption.getContraption().invalidateClientContraptionStructure();
-                                newContraption.getContraption().invalidateClientContraptionChildren();
-                                newContraption.getContraption().invalidateColliders();
-                                newContraption.getContraption().resetClientContraption();
-                            } else {
-                                for (Entity pass : List.copyOf(newContraption.getPassengers())) {
-                                    pass.stopRiding();
-                                }
-
-                                newContraption.stopRiding();
-                                newContraption.discard();
-                            }
-                        }));
-                    }
-                }
+                cVehicle.position().add(0, yOffset, 0),
+                cVehicle.getDeltaMovement(),
+                cVehicle.getYRot(),
+                cVehicle.getXRot(),
+                finalVehicle::set
         ));
 
-        if (cVehicle != null) {
-            cVehicle.changeDimension(new DimensionTransition(
-                    target,
-                    cVehicle.position().add(0, yOffset, 0),
-                    cVehicle.getDeltaMovement(),
-                    cVehicle.getYRot(),
-                    cVehicle.getXRot(),
-                    finalVehicle::set
-            ));
+        int startTick = server.getTickCount();
+        Runnable pollTask = new Runnable() {
+            @Override
+            public void run() {
+                Entity newVehicle = finalVehicle.get();
+                if (newVehicle != null || (server.getTickCount() - startTick) >= 200) {
+                    warpContraption(contraption, target, yOffset, buf, newVehicle, passengers, origin);
+                } else {
+                    server.tell(new TickTask(server.getTickCount() + 1, this));
+                }
+            }
+        };
+
+        server.tell(new TickTask(server.getTickCount() + 1, pollTask));
+    }
+
+    private static void warpContraption(AbstractContraptionEntity prev, ServerLevel target, double yOffset, RegistryFriendlyByteBuf buf, @Nullable Entity newVehicle, Map<UUID, Integer> passengers, ServerLevel origin) {
+        Vec3 targetPos = prev.position().add(0, yOffset, 0);
+        float yRot = prev.getYRot();
+        float xRot = prev.getXRot();
+
+        AbstractContraptionEntity entity = (AbstractContraptionEntity) prev.getType().create(target);
+        prev.discard();
+        if (entity == null || newVehicle == null) return;
+
+        entity.setPos(targetPos);
+        entity.setYRot(yRot);
+        entity.setXRot(xRot);
+        entity.readSpawnData(buf);
+
+        if (entity.getContraption() == null) {
+            utm.LOGGER.warn("[UTM] post-warp contraption null? ???");
+            return;
         }
+
+        target.addFreshEntity(entity);
+        entity.startRiding(newVehicle, true);
+
+        passengers.forEach((uuid, seatIndex) -> {
+            Entity pass = origin.getEntity(uuid);
+
+            if (pass != null)
+                pass.changeDimension(new DimensionTransition(
+                        target,
+                        pass.position().add(0, yOffset, 0),
+                        pass.getDeltaMovement(),
+                        pass.getYRot(),
+                        pass.getXRot(),
+                        (newPass) -> {
+                            entity.addSittingPassenger(newPass, seatIndex);
+                            newPass.addTag("utm_reentry_landing");
+                        }
+                ));
+        });
+    }
+
+    static {
+        utmEvents.register(PlayerTickEvent.Post.class, SpaceStateHandler::onPlayerTick);
+        utmEvents.register(EntityTickEvent.Post.class, SpaceStateHandler::onEntityTick);
+        utmEvents.register(LivingEvent.LivingJumpEvent.class, SpaceStateHandler::onJump);
+        utmEvents.register(LivingFallEvent.class, event -> {
+            Entity entity = event.getEntity();
+
+            if (entity.getTags().contains("utm_reentry_landing")) {
+                event.setDamageMultiplier(0);
+                event.setDistance(0);
+                event.setCanceled(true);
+                entity.removeTag("utm_reentry_landing");
+            }
+        });
     }
 }
