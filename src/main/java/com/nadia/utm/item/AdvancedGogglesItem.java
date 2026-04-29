90720917ca9599acfdb19aa91fa9c75f1ef1773c
nadia <115858781+nadia8666@users.noreply.github.com>
2026-04-28 17:24:31 -0700

    add advanced goggles sealing visuals

--- src/main/java/com/nadia/utm/item/AdvancedGogglesItem.java
+++ src/main/java/com/nadia/utm/item/AdvancedGogglesItem.java
@@ -4,14 +4,19 @@ import com.nadia.utm.registry.item.utmItems;
 import com.simibubi.create.content.equipment.goggles.GogglesItem;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import top.theillusivec4.curios.api.CuriosCapability;
 import top.theillusivec4.curios.api.type.capability.ICuriosItemHandler;
 import top.theillusivec4.curios.api.type.inventory.ICurioStacksHandler;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 public class AdvancedGogglesItem extends GogglesItem {
+    public static final List<Predicate<Player>> ADVANCED_PREDICATES = new ArrayList<>();
     public AdvancedGogglesItem(Properties properties) {
         super(properties);
     }
@@ -20,8 +25,22 @@ public class AdvancedGogglesItem extends GogglesItem {
         return Optional.ofNullable(entity.getCapability(CuriosCapability.INVENTORY)).map(ICuriosItemHandler::getCurios);
     }
 
+    public static synchronized void addIsWearingPredicate(Predicate<Player> predicate) {
+        ADVANCED_PREDICATES.add(predicate);
+        GogglesItem.addIsWearingPredicate(predicate);
+    }
+
+    public static boolean isWearingAdvancedGoggles(Player player) {
+        for (Predicate<Player> predicate : ADVANCED_PREDICATES) {
+            if (predicate.test(player)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     static {
-        GogglesItem.addIsWearingPredicate(p -> resolveCuriosMap(p)
+        addIsWearingPredicate(p -> resolveCuriosMap(p)
                 .map(curiosMap -> {
                     for (ICurioStacksHandler stacksHandler : curiosMap.values()) {
                         int slots = stacksHandler.getSlots();
@@ -36,6 +55,6 @@ public class AdvancedGogglesItem extends GogglesItem {
                 })
                 .orElse(false));
 
-        GogglesItem.addIsWearingPredicate(p -> utmItems.GOGGLES.isIn(p.getItemBySlot(EquipmentSlot.HEAD)));
+        addIsWearingPredicate(p -> utmItems.GOGGLES.isIn(p.getItemBySlot(EquipmentSlot.HEAD)));
     }
 }
