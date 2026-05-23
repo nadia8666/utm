50a260163a33ea12cad3c93f1e52cb158e17a6e0
nadia <115858781+nadia8666@users.noreply.github.com>
2026-05-22 17:57:19 -0700

    fix spear crash

--- src/main/java/com/nadia/utm/item/ThrowingSpearItem.java
+++ src/main/java/com/nadia/utm/item/ThrowingSpearItem.java
@@ -31,13 +31,17 @@ public class ThrowingSpearItem extends Item implements ProjectileItem {
         super(properties);
     }
 
-    public static ItemAttributeModifiers createAttributes(Float damage, Float speed) {
+    public static ItemAttributeModifiers createAttributes(float damage, float speed) {
         return ItemAttributeModifiers.builder()
                 .add(
-                        Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_ID, damage, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND
+                        Attributes.ATTACK_DAMAGE,
+                        new AttributeModifier(Item.BASE_ATTACK_DAMAGE_ID, damage, AttributeModifier.Operation.ADD_VALUE),
+                        EquipmentSlotGroup.MAINHAND
                 )
                 .add(
-                        Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_ID, speed, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND
+                        Attributes.ATTACK_SPEED,
+                        new AttributeModifier(Item.BASE_ATTACK_SPEED_ID, speed, AttributeModifier.Operation.ADD_VALUE),
+                        EquipmentSlotGroup.MAINHAND
                 )
                 .build();
     }
