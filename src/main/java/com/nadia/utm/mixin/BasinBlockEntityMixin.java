74f08c7619f7a6e6cfc8f4bc7194c5a0170eee48
pikmi <pikmiadventurer@gmail.com>
2026-05-25 15:25:34 -0400

    d10: cooling the antiwater

--- src/main/java/com/nadia/utm/mixin/BasinBlockEntityMixin.java
+++ src/main/java/com/nadia/utm/mixin/BasinBlockEntityMixin.java
@@ -2,9 +2,12 @@ package com.nadia.utm.mixin;
 
 import com.nadia.utm.block.misc.large_basin.LargeBasinBlockEntity;
 import com.nadia.utm.registry.fluid.utmFluids;
+import com.simibubi.create.AllFluids;
 import com.simibubi.create.content.processing.basin.BasinBlockEntity;
 import com.simibubi.create.foundation.blockEntity.behaviour.fluid.SmartFluidTankBehaviour;
+import net.minecraft.util.Mth;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.material.Fluids;
 import net.neoforged.neoforge.fluids.FluidStack;
 import net.neoforged.neoforge.fluids.capability.IFluidHandler;
 import org.spongepowered.asm.mixin.Mixin;
@@ -36,19 +39,35 @@ public class BasinBlockEntityMixin {
     private void utm$tick(CallbackInfo ci) {
         BasinBlockEntity basin = (BasinBlockEntity) (Object) this;
         if (basin.getLevel() == null || basin.getLevel().isClientSide) return;
-
+        int tnAntiwater = 0;
+        int tnWater = 0;
+        int tnExperiecne = 0;
         for (SmartFluidTankBehaviour tank : basin.getTanks()) {
             IFluidHandler capability = tank.getCapability();
             for (int i = 0; i < capability.getTanks(); i++) {
                 FluidStack stack = capability.getFluidInTank(i);
                 if (stack.is(utmFluids.ANTIWATER)) {
-                    int rand = basin.getLevel().getRandom().nextIntBetweenInclusive(1, basin instanceof LargeBasinBlockEntity ? 6666 : 666);
+                    tnAntiwater += stack.getAmount(); //plus equal because there will inevitably be Some old poor old basin with
+                    //some water in output and Stuf.
+                } else if (stack.is(Fluids.WATER)) {
+                    tnWater += stack.getAmount();
 
-                    if (rand == 1) {
-                        basin.getLevel().explode(null, basin.getBlockPos().getX() + 0.5D, basin.getBlockPos().getY() + 0.5D, basin.getBlockPos().getZ() + 0.5D, 12F, true, Level.ExplosionInteraction.BLOCK);
-                        return;
-                    }
+                } else if (stack.is(AllFluids.CHOCOLATE)) { // tough find!
+                    tnExperiecne += stack.getAmount(); //..not experience
                 }
+            }
+
+        }
+        if (tnAntiwater > 0) {
+            boolean lb = (basin instanceof LargeBasinBlockEntity);
+            boolean cooled = (tnExperiecne > (tnWater*2)+tnAntiwater);
+            boolean wp = (tnWater > 0);
+            int finalChance = Mth.floor(1000 * (lb ? 10 : 1) * (cooled ? 30 : 1) / (wp ? 0.5 : 1));
+            int rand = basin.getLevel().getRandom().nextIntBetweenInclusive(1, finalChance);
+            // if water present reduce to 10
+            if (rand == 1) {
+                basin.getLevel().explode(null, basin.getBlockPos().getX() + 0.5D, basin.getBlockPos().getY() + 0.5D, basin.getBlockPos().getZ() + 0.5D, 12F, true, Level.ExplosionInteraction.BLOCK);
+                return;
             }
         }
     }
