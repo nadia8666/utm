926d57a4cd3ae0b68e1a37ea39d6eaad3bd0d874
pikmi <pikmiadventurer@gmail.com>
2026-06-10 22:50:06 -0400

    trident stuff 2.0

--- src/main/java/com/nadia/utm/registry/model/utmModels.java
+++ src/main/java/com/nadia/utm/registry/model/utmModels.java
@@ -25,7 +25,7 @@ public class utmModels {
     public static final PartialModel THRUST_LARGE_1 = PartialModel.of(utm.key("block/thrust_large1"));
     public static final PartialModel THRUST_LARGE_2 = PartialModel.of(utm.key("block/thrust_large2")); //Put citywalls here methinks // No
 
-    public static final PartialModel ARID_TRIDENT = PartialModel.of(utm.key("item/arid_trident_in_hand"));
+    public static final PartialModel ARID_TRIDENT = PartialModel.of(utm.key("item/arid_trident_shaft"));
     public static final PartialModel ARID_TRIDENT_E = PartialModel.of(utm.key("item/arid_trident_glow"));
 
     public static final Map<String, PartialModel> THROWING_SPEAR_MODELS = new HashMap<>();
