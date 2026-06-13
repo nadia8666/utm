2ea7e283bbd26fa5da1a7ebc4ba87b891b6c1b2f
pikmi <pikmiadventurer@gmail.com>
2026-06-13 15:29:43 -0400

    noob

--- src/main/java/com/nadia/utm/item/AridSlingshotItem.java
+++ src/main/java/com/nadia/utm/item/AridSlingshotItem.java
@@ -3,6 +3,7 @@ package com.nadia.utm.item;
 import com.nadia.utm.entity.projectile.AridStoneEntity;
 import com.nadia.utm.util.EnchantUtil;
 import net.minecraft.client.resources.sounds.Sound;
+import net.minecraft.core.Holder;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
@@ -11,6 +12,7 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
@@ -25,7 +27,9 @@ public class AridSlingshotItem extends Item {
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
         ItemStack itemStack = player.getItemInHand(usedHand);
-        level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ARROW_SHOOT, null, 0.5f, 1f);
+        int multishot = itemStack.getEnchantmentLevel(level.registryAccess().holderOrThrow(Enchantments.MULTISHOT));;
+
+        level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ARROW_SHOOT, player.getSoundSource(), 0.5f, 1f);
         player.getCooldowns().addCooldown(this,2);
         if (!level.isClientSide) {
             AridStoneEntity gummyballentity = new AridStoneEntity(level, player);
@@ -33,9 +37,20 @@ public class AridSlingshotItem extends Item {
             gummyballentity.shootFromRotation(player,  player.getYRot(), player.getXRot(), 0.0f, 1f, 0f);
             level.addFreshEntity(gummyballentity);
         }
+        if (multishot>=1) {
+            for (int i =1; i <= multishot; i++ ) {
+                for (int u = 1; u<=2; u++) { //DOUBLE FOR LOOPS :O
+                    AridStoneEntity gummyballentity = new AridStoneEntity(level, player);
+                    gummyballentity.setItem(itemStack);
+                    gummyballentity.shootFromRotation(player, player.getYRot(), player.getXRot()+((45f/multishot)*i*((-1)^u)), 0.0f, 1f, 0f);
+                    level.addFreshEntity(gummyballentity);
+                }
+            }
+        }
+
         player.awardStat(Stats.ITEM_USED.get(this));
         if (!player.getAbilities().invulnerable) { //formerly creative mode..
-            itemStack.hurtAndBreak(2, player, EquipmentSlot.MAINHAND);
+            itemStack.hurtAndBreak(1+(multishot==0 ? 0 : (multishot*2)+1), player, EquipmentSlot.MAINHAND);
             //so THIS is how hurtAndBreak works..
         }
         return InteractionResultHolder.success(itemStack);    }
