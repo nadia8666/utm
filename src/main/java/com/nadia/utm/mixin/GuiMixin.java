6f72825ade8d09c3037971f058ccb74dcc285897
pikmi <pikmiadventurer@gmail.com>
2026-07-14 13:18:07 -0400

    NADIA PLEASE FOR THE LOVE OF GOD LOOK AT GUIMIXIN AND MAKE SURE IT DOESNT SUCK.
    Signed-off-by: pikmi <pikmiadventurer@gmail.com>

--- src/main/java/com/nadia/utm/mixin/GuiMixin.java
+++ src/main/java/com/nadia/utm/mixin/GuiMixin.java
@@ -4,16 +4,29 @@ import com.llamalad7.mixinextras.expression.Definition;
 import com.llamalad7.mixinextras.expression.Expression;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.nadia.utm.registry.item.tool.utmTools;
+import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.TickRateManager;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import org.checkerframework.checker.signature.qual.Identifier;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = Gui.class, remap = true)
+@Mixin(value = Gui.class, remap = false)
 public class GuiMixin {
 
     private Minecraft minecraft;
@@ -38,10 +51,6 @@ public class GuiMixin {
  //Big Mambo rewrite this code so its not poop and stupid
 
 
-     @Definition(id = "crosshairPickEntity", field = "Lnet/minecraft/client/Minecraft;crosshairPickEntity:Lnet/minecraft/world/entity/Entity;")
-      @Definition(id = "minecraft", field = "Lnet/minecraft/client/gui/Gui;minecraft:Lnet/minecraft/client/Minecraft;")
-      @Definition(id = "player", field = "Lnet/minecraft/client/Minecraft;player:Lnet/minecraft/client/player/LocalPlayer;")
-      @Definition(id = "LivingEntity", type = LivingEntity.class)
      @Definition(id = "f", local = @Local(type = float.class))
      @Expression("f < 1.0")
       @ModifyExpressionValue(method = "renderCrosshair", at = @At(value = "MIXINEXTRAS:EXPRESSION"))
@@ -49,4 +58,23 @@ public class GuiMixin {
         // still not what i wanted so i am going to kill myself
          return original || (this.minecraft.player.getInventory().getSelected().is(utmTools.SWORD_OF_KIRK.ITEM()));
      }
+
+
+
+     //SLOW, MAYBE TANKS FRAMES.. SLOW!!
+    @Unique private static final ResourceLocation CROSSHAIR_ATTACK_INDICATOR_FULL_TEXTURE = ResourceLocation.parse("hud/crosshair_attack_indicator_full");
+    @Inject(method = "renderCrosshair", at = @At(value = "TAIL"))
+    private void enhancedAttackIndicator$showPlus(GuiGraphics guiGraphics, DeltaTracker deltaTracker, CallbackInfo ci) {
+        if (this.minecraft.player.getInventory().getSelected().is(utmTools.SWORD_OF_KIRK.ITEM())) {
+            RenderSystem.enableBlend();
+            RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.ONE_MINUS_DST_COLOR, GlStateManager.DestFactor.ONE_MINUS_SRC_COLOR, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+            int j = guiGraphics.guiHeight() / 2 - 7 + 16;
+            int k = guiGraphics.guiWidth() / 2 - 8;
+            guiGraphics.blitSprite(CROSSHAIR_ATTACK_INDICATOR_FULL_TEXTURE, k, j, 16, 16);
+            RenderSystem.defaultBlendFunc();
+            RenderSystem.disableBlend();
+
+        }
+    }
+
 }
